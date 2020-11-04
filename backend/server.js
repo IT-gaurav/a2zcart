997549cc4633c91f82747c244c51cbe4d11f2136
IT-gaurav <56316270+IT-gaurav@users.noreply.github.com>
2020-11-04 15:34:10 -0500

    error handling for mongoose

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import colors from "colors";
 import products from "./data/products.js";
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
@@ -17,20 +18,8 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 
-app.use((req, res, next) => {
-  const error = new Error(`Not Found- ${req.originalUrl}`);
-  res.status(404);
-  next(error);
-});
-
-app.use((err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
-  });
-});
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
