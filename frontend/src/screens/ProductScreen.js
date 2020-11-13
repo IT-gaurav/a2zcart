8b4cf57c73968c73079e82dd5422fa7acf0d118b
Gaurav Kumar <gaurav9113@gmail.com>
2020-11-12 20:33:26 -0500

    implemented jwtoken

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -27,7 +27,7 @@ const ProductScreen = ({ history, match }) => {
   }, [dispatch, match]);
 
   const addToCartHandler = () => {
-    history.push(`/cart/${match.params.id} ? qty =${qty}`);
+    history.push(`/cart/${match.params.id}?qty=${qty}`);
   };
 
   return (
